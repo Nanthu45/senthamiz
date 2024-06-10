194be2265f38cb46cabd5408fdad1f1a79ea6dcf
chitharthan-stack <sidhupkc@gmail.com>
2024-06-10 11:47:04 +0530

    password

--- userauths/forms.py
+++ userauths/forms.py
@@ -3,10 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from userauths.models import User
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"username"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"username" }))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder":"email"}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Enter Password"}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Confirm Password"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Enter Password","class":"password-input"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Confirm Password","class":"password-input"}))
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
